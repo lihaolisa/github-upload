58d446baed176d26b040276e19398c2d91236f90
Hao Li <lihaolisa@gmail.com>
2020-04-10 22:27:35 -0700

    path

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -3,6 +3,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 from  run_pred_realtime import pred_all_segments_prob
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -20,7 +21,7 @@ def heatmap():
 @app.route('/route_plan',methods=['POST'])
 def route_plan():
     data = request.get_json(force=True)
-    model = pickle.load(open('model.pkl', 'rb'))
+    model = pickle.load(open(os.getcwd(), 'model.pkl', 'rb'))
     prediction = model.predict([np.array(list(data.values()))])
     output = prediction[0]
     return jsonify(output)
